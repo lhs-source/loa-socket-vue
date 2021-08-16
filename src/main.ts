ae6dee33f3dbab6f7f519c80f7b5987cfa5b5456
hslee <iddms5446@gmail.com>
2021-08-16 13:05:49 +0900

    gtag 추가

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,20 @@ import router from "./router";
 import store from "./store";
 
 import Notifications from 'vue-notification'
+import VueGtag from "vue-gtag";
 
 Vue.use(Notifications);
+
+// GA 설정
+let GA_MID = {
+  id: 'G-3DFJ0YM1SS',
+}
+GA_MID.id = process.env.NODE_ENV === 'prod' ? 'G-3DFJ0YM1SS' : '';
+Vue.use(VueGtag, {
+  config: {
+    id: GA_MID.id
+  }
+}, router);
 
 Vue.config.productionTip = false;
 
